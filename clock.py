08149e22121d0a5894e1fb1aca211dbbf898dd89
Phisit011 <366347221011-st@rmutsb.ac.th>
2024-05-21 11:30:19 +0700

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,35 @@
+from kivy.uix.filechooser import Screen
+from kivy.uix.filechooser import ScreenManager
+from kivy.app import App
+from kivy.clock import Clock
+
+class UI(ScreenManager):
+    pass
+
+class Logo_page(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        Clock.schedule_once(self.go_to_screen, 5)
+    
+    def go_to_screen(self,sec):
+        self.manager.current = "Page1"
+        
+
+class Page1(Screen):
+    count =0
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        Clock.schedule_interval(self.update_label,1)
+
+    def update_label(self,dt):
+
+            self.count+=1
+
+            self.ids.lbl_count.text = str(self.count)
+
+class clockApp(App):
+    def build(self):            
+        return UI()
+    
+if __name__=="__main__":
+    clockApp().run()
